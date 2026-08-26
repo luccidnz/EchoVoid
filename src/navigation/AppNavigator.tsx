c023731485e5a4ce43411fbff43fec8e955f7764
Luccid <luccid.nz@gmail.com>
2026-08-27 03:02:17 +1200

    fix(startup): lazy-load audio and filesystem routes

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,9 +4,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import type { Ech0Mode } from '../ech0void/types';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import HomeScreen from '../screens/HomeScreen';
-import TransmissionScreen from '../screens/TransmissionScreen';
-import LogbookScreen from '../screens/LogbookScreen';
-import SessionDetailScreen from '../screens/SessionDetailScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import { voidTheme } from '../ech0void/theme';
 
@@ -32,15 +29,30 @@ const navigationTheme = {
   },
 };
 
+function TransmissionRoute(props: any) {
+  const Screen = require('../screens/TransmissionScreen').default;
+  return <Screen {...props} />;
+}
+
+function LogbookRoute(props: any) {
+  const Screen = require('../screens/LogbookScreen').default;
+  return <Screen {...props} />;
+}
+
+function SessionDetailRoute(props: any) {
+  const Screen = require('../screens/SessionDetailScreen').default;
+  return <Screen {...props} />;
+}
+
 export default function AppNavigator() {
   return (
     <NavigationContainer theme={navigationTheme}>
       <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false, animation: 'fade' }}>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Transmission" component={TransmissionScreen} />
-        <Stack.Screen name="Logbook" component={LogbookScreen} />
-        <Stack.Screen name="SessionDetail" component={SessionDetailScreen} />
+        <Stack.Screen name="Transmission" component={TransmissionRoute} />
+        <Stack.Screen name="Logbook" component={LogbookRoute} />
+        <Stack.Screen name="SessionDetail" component={SessionDetailRoute} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
